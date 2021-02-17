f07d2171643ad0bd51c0559cb3f68431b2d1ea71
faisal212 <faisalaqdas@gmail.com>
2021-02-17 18:30:19 +0500

    more

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,18 +5,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    { 
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        develop: false,
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
-    },
+
 
     "gatsby-transformer-sharp",
     {
